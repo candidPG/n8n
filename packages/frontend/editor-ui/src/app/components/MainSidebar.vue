46dafa3a678a19b9c3eab3990adfa6044e479eff
Pedro Gonçalves <pedro.goncalves@candidplatform.com>
2026-03-25 18:54:12 +0000

    Side bar menu logo

--- packages/frontend/editor-ui/src/app/components/MainSidebar.vue
+++ packages/frontend/editor-ui/src/app/components/MainSidebar.vue
@@ -404,27 +404,17 @@ useKeybindings({
 	border-top: var(--border);
 }
 
-# CANDID
-.logoItem {
-	display: flex;
-	justify-content: space-between;
-	height: $header-height;
-	line-height: $header-height;
-	margin: 0 !important;
-	border-radius: 0 !important;
-	border-bottom: var(--border-width-base) var(--border-style-base) var(--color-background-xlight);
-	cursor: default;
-
-	&:hover, &:global(.is-active):hover {
-		background-color: initial !important;
-	}
-
-	* { vertical-align: middle; }
-	.icon {
-		height: 18px;
-		position: relative;
-		left: 6px;
-	}
+# CANDID #
+# Side bar menu logo
+._logo_1bvb0_143 ._logoText_bk7tg_131 {
+	width: 51px;
+	height: 12px;
+	margin-left: 5px;
+	margin-top: 4px;
 }
-# CANDID
+._logo_1bvb0_143 ._logoText_bk7tg_131 img {
+	width: 51px;
+	height: 12px;
+}
+# CANDID #
 </style>
